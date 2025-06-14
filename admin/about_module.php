6a370959fa0adca6be3f0d80c1d5db742376df9f
Nicolas SILOBRE <45969285+ns-info90@users.noreply.github.com>
2025-06-14 14:23:44 +0200

    4.0.1
    ## 4.0.0 - 15/05/2025
    - Création d'un setup
    - Options:
      - Afficher la version du module visible (si possible)
      - Désactiver la recherche de la version des modules
    
    - Ajout d'une fonctionnalité permettant au module GMAO d'accèder à la partie admin avec un droit particulier
    
    ## 3.0.1 - 23/04/2025
    - Ajout d'un  champ textarera pour les réglages
    
    ## 3.0.0 - 06/04/2025
    - Mise à jour des info SARL
    - Changement de l'icône du ChangeLog
    - Modification de la partie Support
    - Modification de l'A Propos (page commune des modules)

--- admin/about_module.php
+++ admin/about_module.php
@@ -40,9 +40,9 @@ if (!$res) die("Include of main fails");
 require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
 dol_include_once('/nsinfo/lib/nsinfo.lib.php');
+dol_include_once('/nsinfo/lib/setup.lib.php');
 
-// Access control
-if (!$user->admin) accessforbidden();
+
 
 // Parameters
 $action = GETPOST('action', 'alpha');
@@ -50,13 +50,38 @@ $backtopage = GETPOST('backtopage', 'alpha');
 $namemodule = GETPOST('namemodule', 'alpha');
 
 $modM = ucfirst($namemodule);
+$nameMod = 'mod'.$modM;
+if ($namemodule == 'factory') {
+	dol_include_once('/'.$namemodule.'/core/lib/'.$namemodule.'.lib.php');
+}
+else {
+	dol_include_once('/'.$namemodule.'/lib/'.$namemodule.'.lib.php');
+}
+dol_include_once('/'.$namemodule.'/core/modules/'.$nameMod.'.class.php');
 
-if ($namemodule == 'factory') dol_include_once('/'.$namemodule.'/core/lib/'.$namemodule.'.lib.php');
-else dol_include_once('/'.$namemodule.'/lib/'.$namemodule.'.lib.php');
 
-
+// Access control
+if ($namemodule == 'gmao') {
+	$permissiontoread=$user->rights->gmao->gmao->admin;
+	nsinfo_check_access($permissiontoread);
+}
+else if (!$user->admin) accessforbidden();
 // Translations
-$langs->loadLangs(array("errors", "admin", "nsinfo@nsinfo", "${namemodule}@${namemodule}"));
+$langs->loadLangs(array("errors", "admin", "nsinfo@nsinfo", "{$namemodule}@{$namemodule}"));
+
+$modClass = new $nameMod($db);
+$constantLastVersion = !empty($modClass->getVersion()) ? $modClass->getVersion() : 'NC';
+$supportvalue = "/*****"."<br>";
+$supportvalue.= " * Module : ".$modClass->name." / ".$langs->trans('Module'.$modClass->name.'Name')."<br>";
+$supportvalue.= " * Module version : ".$constantLastVersion."<br>";
+$supportvalue.= " * Dolibarr version : ".DOL_VERSION."<br>";
+$supportvalue.= " * Dolibarr version installation initiale : ".$conf->global->MAIN_VERSION_LAST_INSTALL."<br>";
+$supportvalue.= " * Option colonne sélection à gauche : ".(isset($conf->global->MAIN_CHECKBOX_LEFT_COLUMN) && (!empty($conf->global->MAIN_CHECKBOX_LEFT_COLUMN)) ? $conf->global->MAIN_CHECKBOX_LEFT_COLUMN : 0)."<br>";
+$supportvalue.= " * Version PHP : ".PHP_VERSION."<br>";
+$supportvalue.= " *****/"."<br>";
+$supportvalue.= "Description de votre problème :"."<br>";
+
+
 
 /*
  * Actions
@@ -71,36 +96,55 @@ $langs->loadLangs(array("errors", "admin", "nsinfo@nsinfo", "${namemodule}@${nam
 
 $form = new Form($db);
 
-$page_name = "About";
+$module=$langs->trans("Module".ucfirst("{$namemodule}")."Name");
+
+$page_name = $module . " - ".$langs->trans("About");
+
 llxHeader('', $langs->trans($page_name));
 
 // Subheader
 $linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
 
-print load_fiche_titre($langs->trans($page_name), $linkback, "object_${namemodule}@${namemodule}");
+print load_fiche_titre($langs->trans($page_name), $linkback, "object_{$namemodule}@{$namemodule}");
 
 // Configuration header
 if ($namemodule == 'factory') $namehead = 'factory_admin_prepare_head';
 else $namehead = $namemodule."AdminPrepareHead";
 $head = $namehead();
 
-print dol_get_fiche_head($head, 'about', $langs->trans("About"), -1, "${namemodule}@${namemodule}");
+print dol_get_fiche_head($head, 'about', $langs->trans("About"), -1, "{$namemodule}@{$namemodule}");
 
 dol_include_once('/'.$namemodule.'/core/modules/mod'.ucfirst($namemodule).'.class.php');
 
 $nameMod = "mod".$modM;
 $tmpmodule = new $nameMod($db);
+print '<form id="ticket" method="POST" target="_blank" action="https://gestdoli.ns-info90.fr/public/ticket/create_ticket.php">';
+print '<input name=message type="hidden" value="'.$supportvalue.'" />';
+print '<input name=type_code type="hidden" value="2" />';
+print '<input name=severity_code type="hidden" value="NORMAL" />';
+print '<input name=email type="hidden" value="'.$user->email.'" />';
 print '<a href="https://www.ns-info.fr" border=0 target="_blank"><img src="../img/nsinfo.png" width="200" align="right"></a>';
 print '<table width="100%"><tr>' . "\n";
-print '<td width="310px"><a href="https://www.ns-info.fr" border=0 target="_blank"><img src="../img/nsinfo.png" width="300" /></a></td>' . "\n";
+print '<td width="310px"><a href="https://www.ns-info.fr" border=0 target="_blank"><img src="../img/nsinfo.png" width="300" /><br>';
+print '<div style="text-align: right"><img src="../img/doli-pp.png" width="150" /></div>';
+print '</a></td>' . "\n";
+print '<td>&nbsp;&nbsp;</td>';
 print '<td align="left" valign="top"><p>' . $langs->trans("NSINFOAboutDesc1") .
-											$langs->trans("NSINFOAboutDesc2").
-											$langs->trans("NSINFOAboutDesc3").
-											$langs->trans("NSINFOAboutDesc4");
+	$langs->trans("NSINFOAboutDesc2").
+	$langs->trans("NSINFOAboutDesc3").
+	$langs->trans("NSINFOAboutDesc4").' '.
+	'<button type="submit">'.$langs->trans("NSINFOAboutDesc45").'</button>';
 if (strlen($langs->transnoentities("NSINFOAboutDesc5")) > 16 && $namemodule == 'gmao') print $langs->transnoentities("NSINFOAboutDesc5");
 print '</p></td>' . "\n";
 
-print '</tr></table>' . "\n";
+print '</tr><tr>';
+print '<td width="310px">&nbsp;</td>' . "\n";
+print '<td>&nbsp;&nbsp;</td>';
+print '<td align="left" valign="top"><p>'. $langs->trans("NSINFOAboutDescDolistore").' <a href="https://www.dolistore.com/index.php?controller=search&orderby=position&orderway=desc&tag=&website=marketplace&search_query=NSINFO" border=0 target="_blank"><img src="../img/dolistore.png" width="100" /></a>';
+
+print '</tr>';
+
+print '</table>' . "\n";
 
 // Page end
 dol_fiche_end();
